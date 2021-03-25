6d356ff7d47afe6b620d5b2aa5b18e165ce9593b
kkatai <73082301+kkatai@users.noreply.github.com>
2021-03-24 21:55:53 -0700

    iterations on 19

--- js/index.js
+++ js/index.js
@@ -1,2 +1,37 @@
+function fade() {
+    $('.preloader').fadeOut("slow");
+    }
+    setTimeout(fade, 3000);
+
+        
+//$('.topRight').hover(function(){
+  //  $(".topRight").attr('src', 'images/about_nondoodle.jpeg')},function(){
+  //  $(".topRight").attr('src', 'images/about_doodle.png');
+//});
+
+// $('.topRight').hover(function(){
+//     $(".topRight").css('background-image', 'url(images/about_nondoodle.jpeg)')},function(){
+//     $(".topRight").attr('background-image', 'url(images/about_doodle.png)');
+// });
+
+// .topRight{
+//     background-image: NON-HOVER IMAGE LINK IS
+// }
+// .topRight:hover{
+//     background-image: HOVER IMAGE LINK IS
+// }
+
+//$("#resumeButton").hover(function(){
+ //   $(".resume").css("background-color", "red")}, function(){
+  //      $(".resume").css("background-color", "#a26b59")}
+//);
+
+
+//$("header ul").on("hover", function() {
+  //  $("header li").css("background-color", "pink");
+//});
+
+
+
 var kaitlynKatai = "hey there!";
 console.log("JS is set up correctly!");
